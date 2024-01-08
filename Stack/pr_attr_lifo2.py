352c8070c6436485b237d26317e23720172bca9b
Beyoung Su Kang <81085595+BPT0@users.noreply.github.com>
2024-01-08 19:46:38 +0900

    feat. lifo 실습2-일 온도 갱신시점 파악

---
+++ Stack/pr_attr_lifo2.py
@@ -0,0 +1,14 @@
+# tuple 및 stack 활용
+# 특정조건이 되면 데이터를 pop 하는 형식
+# stack[-1] 은 top의 데이터를 가리킴
+# prev_day, _ 튜플에 stack의 pop 한 값을 저장
+
+def dailyTemperatures(temp):
+    answer = [0] * len(temp)
+    stack = []
+    for day, temp in enumerate(temp):
+        while stack and stack[-1][1] < temp:
+            prev_day, _ = stack.pop()
+            answer[prev_day] =day - prev_day
+        stack.append((day, temp))
+    return answer
